8858004ac6d1fe949db841f31180bded890815f5
Nir <Nir@Nir-PC>
2010-03-13 16:25:34 +0200

    ????? ???????? ?????? ?? memberType messageType ????? ??? TYPE

--- src/Forum/PersistentLayer/dataParser.java
+++ src/Forum/PersistentLayer/dataParser.java
@@ -14,47 +14,47 @@ import java.util.NoSuchElementException;
  *
  * @author Arseny
  */
-public interface DataParser {
+public interface dataParser {
     // receives a member object and parses it into xml compatible class
     /**
      * add a new memer to the forum
      * @param m a member with all the fields
      * 
      */
-    public void addMember(Member m);
+    public void addMember(MemberType m);
     
    
     /**
      * add a new message or update an existing message
      * @param m a message
      */
-    public void addMessage(Message m);
+    public void addMessage(MessageType m);
 
     /**
      *  find a member by his nickname
      * @param nick a nickname
      * @return the member if such exists and null otherwise
      */
-    public Member getMember(String nick);
+    public MemberType getMember(String nick);
 
     /**
      *  return a list of all members in the forum
      * @return list of members and null if empty
      */
-    public List<Member> getMembers();
+    public List<MemberType> getMembers();
 
     /**
      *  find a message by it's message ID
      * @param id message ID
      * @return a message if such exists and null otherwise
      */
-    public Message getMessage(int id);
+    public MessageType getMessage(int id);
 
     /**
      *  returns all the parent messages in the forum
      * @return list of all the parent messages or null if empty
      */
-    public List<Message> getParentMessages();
+    public List<MessageType> getParentMessages();
 
     /**
      *  returns all of the direct children of a message
@@ -62,7 +62,7 @@ public interface DataParser {
      * @return a list of the direct children of this message or null if no children exists
      * @throws NoSuchElementException  in case no such message exist
      */
-    public List<Message> getChildren(int id) throws NoSuchElementException;
+    public List<MessageType> getChildren(int id) throws NoSuchElementException;
 
 
 }
