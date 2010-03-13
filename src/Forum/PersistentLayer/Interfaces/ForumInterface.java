7c6bb682cc7ffe025fd04c3f5bf99d056786e551
lironkatav <lironkatav@localhost>
2010-03-14 00:12:49 +0200

    (no commit message)

--- src/Forum/PersistentLayer/Interfaces/ForumInterface.java
+++ src/Forum/PersistentLayer/Interfaces/ForumInterface.java
@@ -10,22 +10,6 @@ import java.util.Date;
 public interface ForumInterface {
 	
 	/**
-     * set the status of the user to be online
-     * @param username, password */
-	public void login (String username);
-	
-	/**
-     * set the status of the user to be offline
-     * @param username a message id */
-	public void logoff (String username);
-	
-	/**
-     * check if the username already exist 
-     * @param username 
-     * @return username password if exists or NULL if not*/
-	public String userExist (String username);
-	
-	/**
      * add a new member to the forum
      * @param username ,  nickName, password, eMail, firstName, lastName, dateOfBirth */
 	public void register ( 	String userName , 
@@ -37,7 +21,24 @@ public interface ForumInterface {
 							Date dateOfBirth);
 	
 	/**
+     * check if the username already exists 
+     * @param username 
+     * @return username password if exists or NULL if not*/
+	public String userExist (String username);
+	
+	/**
+     * set the status of the user to be online
+     * @param username, password */
+	public void login (String username);
+	
+	/**
+     * set the status of the user to be offline
+     * @param username a message id */
+	public void logoff (String username);
+		
+	/**
      * add a new message to the forum
      * @param parentId, createdBy, subject,body */
-	public void addMessage ( int parentId, String createdBy, String subject,String body, Date DateAdded);
+	public void addMessage ( int parentId, String createdBy, String subject,String body, Date DateAdded, Date modifyDate);
+
 }
