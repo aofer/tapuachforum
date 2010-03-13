eb4a083dbb1955f5b50505cc7dead448499eccef
arsenikr <arsenikr@gmail.com>
2010-03-13 17:27:12 +0200

    constructor and read function to XMLDataParser added test class forXMLDataParser added and read test made and passed

--- src/Forum/PersistentLayer/XMLDataParser.java
+++ src/Forum/PersistentLayer/XMLDataParser.java
@@ -5,14 +5,38 @@
 
 package Forum.PersistentLayer;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 
 /**
  *
  * @author Arseny
  */
 public class XMLDataParser implements dataParser {
+
+    private ObjectFactory of;
+    private JAXBContext jc;
+    private String fileName;
+
+    public XMLDataParser(String fileName) {
+        this.of = new ObjectFactory();
+        try {
+            this.jc = JAXBContext.newInstance("Forum.PersistentLayer");
+        } catch (JAXBException ex) {
+            Logger.getLogger(XMLDataParser.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        this.fileName = fileName;
+    }
+
+
 
     public void addMember(MemberType m) {
         throw new UnsupportedOperationException("Not supported yet.");
@@ -42,8 +66,28 @@ public class XMLDataParser implements dataParser {
         throw new UnsupportedOperationException("Not supported yet.");
     }
 
-    public Forum ReadFromXML(String file){
-                throw new UnsupportedOperationException("Not supported yet.");
+    public Forum ReadFromXML(){
+        {
+            Forum f = null;
+            FileInputStream in = null;
+            try {
+                Unmarshaller u = jc.createUnmarshaller();
+                in = new FileInputStream(this.fileName);
+                f = (Forum) u.unmarshal(in);
+                in.close();
+            } catch (IOException ex) {
+                Logger.getLogger(XMLDataParser.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (JAXBException ex) {
+                Logger.getLogger(XMLDataParser.class.getName()).log(Level.SEVERE, null, ex);
+            } finally {
+                try {
+                    in.close();
+                } catch (IOException ex) {
+                    Logger.getLogger(XMLDataParser.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            }
+             return f;
+        }
 
     }
 
