dde624b5db228b1cedd181c4e339630671fbe7cf
amit <amit@amit-dell>
2010-03-13 21:21:34 +0200

    [no commit message]

---
+++ src/Forum/DomainLayer/Guest.java
@@ -0,0 +1,21 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Forum.DomainLayer;
+
+/**
+ *
+ * @author amit
+ */
+public class Guest {
+
+
+    public Guest(){
+
+
+    }
+
+    
+}
--- src/Forum/DomainLayer/Message.java
+++ src/Forum/DomainLayer/Message.java
@@ -5,15 +5,68 @@
 
 package Forum.DomainLayer;
 
+import java.util.Date;
 import java.util.Vector;
 
 /**
  *
- * @author Arseny
+ * @author Amit Ofer
  */
 public class Message {
- private String nick;
- private String title;
- private String body;
- private Vector<Message> comments;
+ private int _messageId;
+ private String _nickname;
+ private String _subject;
+ private String _body;
+ private Vector<Message> _replies;
+ private Date _writeDate;
+ private Date _modifiedDate;
+
+ public Message(int messageId,String nickname,String subject,String body){
+     this._messageId = messageId;
+     this._nickname = nickname;
+     this._subject = subject;
+     this._body = body;
+     this._replies  = new Vector<Message>();
+     this._writeDate = new Date();
+     this._modifiedDate = new Date();
+ }
+
+    public int getMessageId() {
+        return _messageId;
+    }
+
+    public String getBody() {
+        return _body;
+    }
+
+    public String getNickname() {
+        return _nickname;
+    }
+
+    public String getSubject() {
+        return _subject;
+    }
+
+    public Vector<Message> getReplies() {
+        return _replies;
+    }
+
+    public void setBody(String _body) {
+        this._body = _body;
+    }
+
+    public void setSubject(String _subject) {
+        this._subject = _subject;
+    }
+
+
+ 
+ public void addReply(Message msg){
+     this._replies.add(msg);
+ }
+ public String toString(){
+     return "Message number:" + _messageId + "\nSubject:" + _subject + "\nWritten by:" + _nickname
+             + "\n Number of replies:" + _replies.size() + "\nBody:" + _body + "\n";
+ }
+
 }
