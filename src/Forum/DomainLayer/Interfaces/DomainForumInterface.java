816a8f79babf75045b6b0087ac989e936a73c311
amit <amit@amit-dell>
2010-03-14 19:51:31 +0200

    added more implementation last update before liron went to her meeting

--- src/Forum/DomainLayer/Interfaces/DomainForumInterface.java
+++ src/Forum/DomainLayer/Interfaces/DomainForumInterface.java
@@ -5,6 +5,7 @@
 
 package Forum.DomainLayer.Interfaces;
 
+import Forum.Exceptions.WrongPasswordException;
 import Forum.DomainLayer.Member;
 import Forum.DomainLayer.Message;
 import Forum.Exceptions.*;
@@ -14,10 +15,11 @@ import Forum.Exceptions.*;
  * @author Amit Ofer
  */
 public interface DomainForumInterface {
-    public Message getMessage(int messageId);
+    public Message getMessage(int messageId)throws MessageNotFoundException;
     public void register(Member newMember) throws UserExistsException,NicknameExistsException,BadPasswordException;
-    public void login(String username,String password);
+    public void login(String username, String password) throws NoSuchUserException,WrongPasswordException ;
     public void logout(String username);
     public void addMessage(String nickname,String Subject,String body);
     public boolean checkPasswordPolicy(String password);
+    public void addMember(Member member) ;
 }
--- src/Forum/PersistentLayer/Interfaces/MessageInterface.java
+++ src/Forum/PersistentLayer/Interfaces/MessageInterface.java
@@ -12,50 +12,74 @@ public interface MessageInterface {
 		/**
 	     * get the parent id number of the message 
 	     * @param messageID a message id
-	     * @returns the parent id  */
+                 * @return
+                 * @throws MessageNotFoundException
+                 * @returns the parent id  */
 		public int getParentId(int messageID) throws MessageNotFoundException;
 				
 		/**
 	     * get the subject of the message 
 	     * @param messageID a message id
-	     * @returns the subject of the message given    */
+                 * @return
+                 * @throws MessageNotFoundException
+                 * @returns the subject of the message given    */
 		public String getSubject(int messageID) throws MessageNotFoundException ;
 				
 		/**
 	     * get the content of the message 
 	     * @param messageID a message id
-	     * @returns the content of the message given    */
+                 * @return
+                 * @throws MessageNotFoundException
+                 * @returns the content of the message given    */
 		public String getContent(int messageID) throws MessageNotFoundException ;
 				
 		/**
 	     * get the username the wrote the messgae 
 	     * @param messageID a message id
-	     * @returns the username the wrote the messgae   */
+                 * @return
+                 * @throws MessageNotFoundException
+                 * @returns the username the wrote the messgae   */
 		public String getUsername(int messageID) throws MessageNotFoundException ;
 		
 		/**
 	     * get the date and time of the message 
 	     * @param messageID a message id
-	     * @returns the  date and time of the messgae   */
+                 * @return
+                 * @throws MessageNotFoundException
+                 * @returns the  date and time of the messgae   */
 		public Date getDateAdded(int messageID) throws MessageNotFoundException ;
 
                 	/**
 	     * get the nickname that wrote the message
 	     * @param messageID a message id
-	     * @returns the username that wrote the message   */
+                         * @return
+                         * @throws MessageNotFoundException
+                         * @returns the username that wrote the message   */
 		public String getNickname(int messageID) throws MessageNotFoundException ;
 
 	//--------------------Setters------------------------------
 		/**
 	     * set the subject of the message to be the new subject 
-	     * @param messageID,newSubject*/
+                 * @param messageID
+                 * @param newSubject
+                 * @throws MessageNotFoundException
+                 */
 		public void setSubject(int messageID, String newSubject) throws MessageNotFoundException ;
 		
 		/**
 	     * set the content of the message to be the new content 
-	     * @param messageID, newContent
-	     * @returns the content of the message given    */
+                 * @param messageID
+                 * @param newContent
+                 * @throws MessageNotFoundException
+                 * @returns the content of the message given    */
 		public void setContent(int messageID, String newContent) throws MessageNotFoundException ;
+
+                /**
+                 * returns the last date that the message was modified
+                 * @param messageId
+                 * @return
+                 */
+                public Date getModifiedDate(int messageId);
 
 
 
