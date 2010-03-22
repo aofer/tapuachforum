0e77ac1dd4f5d281ffcfb7d850b505e41aea2289
amit <amit@amit-dell>
2010-03-22 19:40:39 +0200

    added the getRepliesId to the interface between the layers in the method getMessage added the recursive calls for creating the vector of replies

--- src/Forum/PersistentLayer/Interfaces/XMLMemberInterface.java
+++ src/Forum/PersistentLayer/Interfaces/XMLMemberInterface.java
@@ -8,5 +8,10 @@ import Forum.PersistentLayer.Data.MemberData;
 */
 
 public interface XMLMemberInterface {
+    /**
+     * get the member data of the member
+     * @param userName - the username of the member we need to get
+     * @return - a MemberData object containing all the member's data
+     */
     MemberData getMember(String userName);
 }
