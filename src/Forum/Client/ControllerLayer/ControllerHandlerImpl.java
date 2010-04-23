b18b911884de55f3629bc749833e19852538f4ed
omri hadur <omri.hadur@gmail.com>
2010-04-23 18:30:30 +0300

    Adding some stuff to the controller. notice  that we need Subject in the tree ui

--- src/Forum/Client/ControllerLayer/ControllerHandlerImpl.java
+++ src/Forum/Client/ControllerLayer/ControllerHandlerImpl.java
@@ -1,19 +1,15 @@
 package forum.client.controllerlayer;
 
-import Forum.Settings;
+import Forum.Client.ControllerLayer.ClientConnectionController;
+import Forum.TCPCommunicationLayer.AddMessageMessage;
+import Forum.TCPCommunicationLayer.AddReplyMessage;
 import Forum.TCPCommunicationLayer.ClientMessage;
+import Forum.TCPCommunicationLayer.DeleteMessageMessage;
+import Forum.TCPCommunicationLayer.ModifyMessageMessage;
 import Forum.TCPCommunicationLayer.ServerResponse;
 import java.awt.Component;
 
 import forum.client.ui.events.*;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.net.SocketAddress;
-import java.util.logging.Logger;
 
 /**
  * You need to delete all the code in here and implement it yourself.<br>
@@ -23,71 +19,7 @@ import java.util.logging.Logger;
  */
 public class ControllerHandlerImpl extends ControllerHandler {
 
-    private Socket m_socket;
-    private ObjectInputStream in;
-    private ObjectOutputStream out;
-    private static Logger log = Logger.getLogger(Settings.loggerName);
-
-    /* (non-Javadoc)
-     * @see forumtree.contol.ControllerHandler#getForumView()
-     */
-    public void setConnection(String addr, short port) throws IOException {
-        InetAddress ia = InetAddress.getByName(addr);
-        connect(ia, port);
-    }
-
-    private void connect(InetAddress addr, short port) throws IOException {
-        SocketAddress sa = new InetSocketAddress(addr, port);
-        m_socket = new Socket();
-        m_socket.connect(sa);
-        out = new ObjectOutputStream(m_socket.getOutputStream());
-        out.flush();
-        in = new ObjectInputStream(m_socket.getInputStream());
-        log.info("The client is connected to the server.");
-    }
-
-    private void closeConnection() throws IOException {
-        in.close();
-        out.close();
-        m_socket.close();
-    }
-
-    private void send(ClientMessage msg) {
-        try {
-            /* send the message to the server. */
-            out.writeObject(msg);
-            /* receive response from the server. */
-        } catch (IOException ex) {
-             log.severe("got IOException while sending");
-        }
-    }
-
-    private void listen() {
-        Object o = null;
-
-        try {
-            o = in.readObject();
-        } catch (IOException ex) {
-            log.severe("got IOException while listinnig");
-        } catch (ClassNotFoundException ex) {
-            log.severe("got ClassNotFoundException while listinnig");
-        }
-        if (o == null) {
-            log.severe("Lost connection to server.");
-        } else if (!(o instanceof ServerResponse)) {
-            log.severe("Received an invalid response from server.");
-        } else {
-            ServerResponse res = (ServerResponse) o;
-            /* Check if the server has done the command. */
-            if (res.hasExecuted()) {
-                System.out.println("done!");
-            } else {
-                System.out.println("failed!");
-            }
-            /* Print the response from the server */
-            System.out.println(res.getResponse());
-        }
-    }
+    private ClientConnectionController _connectionController;
 
     @Override
     public String getForumView() {
@@ -95,60 +27,33 @@ public class ControllerHandlerImpl extends ControllerHandler {
     }
 
     @Override
-    public void modifyMessage(long id, String newContent, Component comp) {
-        try {
-            Thread.sleep(1500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        notifyObservers(new ForumTreeRefreshEvent(comp, getForumView()));
-        if (Math.random() > 0.5) {
-            notifyObservers(new ForumTreeErrorEvent("Failed to modify a message"));
-        }
+    public void modifyMessage(long id, String  subject,String body, Component comp) {
+        hanndleEvent(new ModifyMessageMessage(id, subject, body),comp);
     }
 
     @Override
-    public void addReplyToMessage(long id, String string, Component comp) {
-        try {
-            Thread.sleep(1500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        notifyObservers(new ForumTreeRefreshEvent(comp, getForumView()));
-        if (Math.random() > 0.5) {
-            notifyObservers(new ForumTreeErrorEvent("Failed to reply to a message"));
-        }
+    public void addReplyToMessage(long id, String subject,String body, Component comp) {
+        hanndleEvent(new AddReplyMessage(id, subject, body),comp);
     }
 
     @Override
     public void deleteMessage(long id, Component comp) {
-        try {
-            Thread.sleep(1500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-
-        notifyObservers(new ForumTreeRefreshEvent(comp, getForumView()));
-        if (Math.random() > 0.5) {
-            notifyObservers(new ForumTreeErrorEvent("Failed to delete message"));
-        }
-
+        hanndleEvent(new DeleteMessageMessage(id),comp);
     }
 
     @Override
-    public void addNewMessage(Component comp) {
-        try {
-            Thread.sleep(1500);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
+    public void addNewMessage(String subject,String body, Component comp) {
+        hanndleEvent(new AddMessageMessage(subject, body),comp);
+    }
 
-        notifyObservers(new ForumTreeRefreshEvent(comp, getForumView()));
-        if (Math.random() > 0.5) {
-            notifyObservers(new ForumTreeErrorEvent("Failed to add a new message"));
+    private void hanndleEvent(ClientMessage msg, Component comp) {
+        ServerResponse res;
+        _connectionController.send(msg);
+        res = _connectionController.listen();
+        if (res.hasExecuted()) {
+            notifyObservers(new ForumTreeRefreshEvent(comp, getForumView()));
+        } else {
+            notifyObservers(new ForumTreeErrorEvent(res.getResponse()));
         }
-
     }
 }
