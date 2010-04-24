367d7c55a95fb396a495ed1dd1151ef81e857340
amit <amit@amit-dell>
2010-04-24 13:33:09 +0300

    Status Panel

---
+++ src/Forum/DomainLayer/Interfaces/MessageInterface.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Forum.DomainLayer.Interfaces;
+
+import Forum.DomainLayer.Message;
+import java.util.Vector;
+
+/**
+ *
+ * @author Kipi
+ */
+public interface MessageInterface {
+
+    /**
+     * add a new reply
+     * @param msg
+     */
+    void addReply(Message msg);
+
+    /**
+     * getter for the body
+     * @return
+     */
+    String getBody();
+
+    /**
+     * getter for the nickname
+     * @return
+     */
+    String getNickname();
+
+    /**
+     * getter for the replies
+     * @return
+     */
+    Vector<Message> getReplies();
+
+    /**
+     * getter for the subject
+     * @return
+     */
+    String getSubject();
+
+    /**
+     * setter for th e body
+     * @param _body
+     */
+    void setBody(String body);
+
+    /**
+     * setter for the subject
+     * @param _subject
+     */
+    void setSubject(String subject);
+
+     int getIndex();
+}
---
+++ src/Forum/DomainLayer/Message.java
@@ -0,0 +1,137 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Forum.DomainLayer;
+
+import Forum.DomainLayer.Interfaces.MessageInterface;
+import Forum.PersistentLayer.Data.MessageData;
+import java.util.Vector;
+
+/**
+ *
+ * @author Amit Ofer
+ */
+public class Message implements MessageInterface {
+
+    private MessageData _data;
+    private Vector<Message> _replies;
+
+    /**
+     *constructor
+     * @param nickname
+     * @param subject
+     * @param body
+     */
+    public Message(MessageData data) {
+        this._data = data;
+        this._replies = new Vector<Message>();
+    }
+
+    /**
+     *another constructor
+     * @param nickname
+     * @param subject
+     * @param body
+     * @param replies
+     * @param WriteDate
+     * @param ModifiedDate
+     */
+    public Message(MessageData data, Vector<Message> replies) {
+        this(data);
+        this._replies = new Vector<Message>();
+    }
+
+    /**
+     * getter for the body
+     * @return
+     */
+    public String getBody() {
+        return _data.getBody();
+    }
+
+    /**
+     *getter for the nickname
+     * @return
+     */
+    public String getNickname() {
+        return _data.getNickname();
+    }
+
+    /**
+     *getter for the subject
+     * @return
+     */
+    public String getSubject() {
+        return _data.getSubject();
+    }
+
+    /**
+     *getter for the replies
+     * @return
+     */
+    public Vector<Message> getReplies() {
+        return _replies;
+    }
+
+    /**
+     *setter for th e body
+     * @param _body
+     */
+    public void setBody(String body) {
+        _data.setBody(body);
+    }
+
+    /**
+     *setter for the subject
+     * @param _subject
+     */
+    public void setSubject(String subject) {
+        _data.setSubject(subject);
+    }
+
+    /**
+     *add a new reply
+     * @param msg
+     */
+    public void addReply(Message msg) {
+        this._replies.add(msg);
+    }
+
+    /**
+     * toString method for a message
+     * @return
+     */
+    public String toString() {
+        String ans = "MessageId: " + _data.getId() + "\nSubject:" + _data.getSubject() + "\nWritten by:"
+                + _data.getNickname() + "\nBody:" + _data.getBody() + "\n";
+       if (_replies.size() > 0){
+            ans = ans + "\nReplies:\n\n";
+            for (int i = 0; i < _replies.size();i++){
+                ans = ans + "Reply      " + (i+1)  + "\n";
+                ans = ans + _replies.elementAt(i).toString() + "\n";
+            }
+       }
+        return ans;
+     //   return "\nSubject:" + _data.getSubject() + "\nWritten by:" + _data.getNickname()
+      //          + "\n Number of replies:" + _replies.size() + "\nBody:" + _data.getBody() + "\n";
+    }
+
+    /**
+     * index getter
+     * @return the index of the message
+     */
+    public int getIndex() {
+        return this._data.getId();
+    }
+        
+
+    /**
+     * index setter - should not be used; used only for tests.
+     * @param id - the index of the message
+     */
+    public void  setIndex(int id) {
+        this._data.setId(id);
+    }
+
+}
