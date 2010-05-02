5422502769d02d0741af6d5aaa645c777e3fe961
Arseny <Arseny@Arseny-PC>
2010-05-02 14:21:15 +0300

    compass added but not tested.. waiting for working DB infastructure

---
+++ src/Forum/DomainLayer/SearchEngine/CompassInstance.java
@@ -0,0 +1,51 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Forum.DomainLayer.SearchEngine;
+
+import Forum.DomainLayer.Forum;
+import java.io.File;
+import org.compass.core.Compass;
+import org.compass.core.config.CompassConfiguration;
+import org.compass.core.config.CompassConfigurationFactory;
+
+/**
+ *
+ * @author Arseny
+ */
+public class CompassInstance {
+
+
+    private static Compass _compass;
+
+    /**
+     * sort of Singleton design implementation
+     * @return Compass instance if exist, or new if it doesn't
+     */
+    public static Compass getInstance() {
+        if (_compass == null) {
+            _compass = CompassInit();
+
+        }
+        return _compass;
+    }
+
+    /**
+     * private constructor for the Singleton design
+     */
+    private CompassInstance() {
+        _compass = null;
+    }
+
+    private static Compass CompassInit(){
+        File file = new File("compassSettings.xml");
+        /* The genData directory is where the search engine will save its data */
+        CompassConfiguration conf = CompassConfigurationFactory
+                .newConfiguration().configure(file);
+
+        Compass compass = conf.buildCompass();
+        return compass;
+    }
+}
