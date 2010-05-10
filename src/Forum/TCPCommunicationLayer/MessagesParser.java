8c8a52bff618e690397dfa2ba860c379bf4c6c70
Kipi <Kipi@Kipi-PC>
2010-05-10 19:01:30 +0300

    Adding Score ordering and display to the search GUI. NEED TO BE CHECKED

--- src/Forum/TCPCommunicationLayer/MessagesParser.java
+++ src/Forum/TCPCommunicationLayer/MessagesParser.java
@@ -6,6 +6,7 @@ package Forum.TCPCommunicationLayer;
 
 import Forum.DomainLayer.Interfaces.MessageInterface;
 import Forum.DomainLayer.Message;
+import Forum.DomainLayer.SearchEngine.SearchHit;
 import Forum.PersistentLayer.Data.MessageData;
 import java.text.DateFormat;
 import java.text.ParseException;
@@ -22,19 +23,27 @@ public class MessagesParser {
 
     static DateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
 
-    public static String Encode(MessageInterface msg) {
-        String str;
-        //DateFormat  formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+    public static String Encode(SearchHit msg) {
+        return MessagesParser.Encode(msg.getMessage(), "score:" + msg.getScore());
+    }
 
+    public static String Encode(MessageInterface msg) {
+        return MessagesParser.Encode(msg, "");
+    }
+
+    private static String Encode(MessageInterface msg, String exstra) {
+        String str;
         str = " <message>";
+        str += exstra;
         str += "id:" + msg.getIndex();
         str += "user:" + msg.getNickname();
         str += "subject:" + msg.getSubject();
         str += "body:" + msg.getBody();
-        if (msg.getWriteDate() == null)
-                str += "WriteDate:" + formatter.format(new Date());
-        else
-        str += "WriteDate:" + formatter.format(msg.getWriteDate());
+        if (msg.getWriteDate() == null) {
+            str += "WriteDate:" + formatter.format(new Date());
+        } else {
+            str += "WriteDate:" + formatter.format(msg.getWriteDate());
+        }
         str += "ModifiedDate:" + formatter.format(msg.getModifiedDate());
 
         for (Iterator<Message> it = msg.getReplies().iterator(); it.hasNext();) {
@@ -43,6 +52,32 @@ public class MessagesParser {
         }
         str += " </message>";
         return str;
+    }
+
+    public static Vector<SearchHit> DecodeHits(String str) {
+        int len = str.length();
+        int pos = 1;
+        int msgStart;
+        int msgEnd;
+        Vector<SearchHit> hitVec = new Vector<SearchHit>();
+        SearchHit hit;
+
+        while (pos < len) {
+            msgStart = str.indexOf("<message>", pos + 1);
+            msgEnd = str.indexOf("</message>", pos);
+            if (msgStart == -1 | msgEnd < msgStart) {
+                hit = getHit(str.substring(pos + 9, msgEnd));
+                hitVec.add(hit);
+                pos = msgEnd + 11;
+            } else {
+                hit = getHit(str.substring(pos + 9, msgStart));
+                msgEnd = getClosed(msgStart, str);
+                hit.getMessage().addReply(Decode(str.substring(msgStart - 1, msgEnd)));
+                hitVec.add(hit);
+                pos = msgEnd + 11;
+            }
+        }
+        return hitVec;
     }
 
     public static Vector<Message> Decode(String str) {
@@ -62,10 +97,10 @@ public class MessagesParser {
                 pos = msgEnd + 11;
             } else {
                 msg = getMessage(str.substring(pos + 9, msgStart));
-                msgEnd=getClosed(msgStart,str);
-                msg.addReply(Decode(str.substring(msgStart - 1,msgEnd)));
+                msgEnd = getClosed(msgStart, str);
+                msg.addReply(Decode(str.substring(msgStart - 1, msgEnd)));
                 msgVec.add(msg);
-                pos =msgEnd+11;
+                pos = msgEnd + 11;
             }
         }
         return msgVec;
@@ -79,7 +114,7 @@ public class MessagesParser {
             msgStart = str.indexOf("<message>", index);
             msgEnd = str.indexOf("</message>", index);
 
-            if (msgStart!=-1 & msgStart < msgEnd) {
+            if (msgStart != -1 & msgStart < msgEnd) {
                 count++;
                 index = msgStart + 1;
             } else {
@@ -94,11 +129,18 @@ public class MessagesParser {
         return 0;
     }
 
-    private static Message getMessage(String str) {
-        int tmpIndex1 = str.indexOf("user:");
-        int tmpIndex2 = str.indexOf("subject:");
+    private static SearchHit getHit(String str) {
+        Message msg = getMessage(str);
+        double score = Double.parseDouble(str.substring(6, str.indexOf("id:")));
+        return new SearchHit(msg, score);
+    }
 
-        int id = Integer.parseInt(str.substring(3, tmpIndex1));
+    private static Message getMessage(String str) {
+        int tmpIndex1 = str.indexOf("id:");
+        int tmpIndex2 = str.indexOf("user:");
+
+        int id = Integer.parseInt(str.substring(tmpIndex1 + 3, tmpIndex2));
+        tmpIndex2 = str.indexOf("subject:");
         String user = str.substring(tmpIndex1 + 5, tmpIndex2);
         tmpIndex1 = str.indexOf("body:");
         String sub = str.substring(tmpIndex2 + 8, tmpIndex1);
